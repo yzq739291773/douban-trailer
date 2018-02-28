dc083e95240f81c8b76ef75cbdb770b5ae78ac9e
yuzq <yuzq@34era.com>
2018-02-28 16:25:56 +0800

    服务器返回一个静态的HTML页面

---
+++ server/tpl/index.js
@@ -0,0 +1,4 @@
+const normaltpl = require('./normal.js')
+module.exports = {
+    normal:normaltpl
+}
---
+++ server/tpl/normal.js
@@ -0,0 +1,26 @@
+module.exports = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+        <div class="col-md-8">
+            <h1>Hi Koa2</h1>
+            <p>This is yzq</p>
+        </div>
+        <div class="col-md-4">
+            <p>测试静态 HTML 页面</p>
+        </div>
+        </div>
+    </div>
+</body>
+</html>`
